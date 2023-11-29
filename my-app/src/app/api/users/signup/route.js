e7cc71c2975f00c1392944174565ade4c06e29bf
captain-Arvoy <accofstardust@gmail.com>
2023-11-29 21:52:12 +0530

    Signup is online

--- my-app/src/app/api/users/signup/route.js
+++ my-app/src/app/api/users/signup/route.js
@@ -5,33 +5,36 @@ import User from "@/models/userModel";
 connect()
 export async function POST(NextRequest){
     try{
-        const reqBody=await Request.json()
+        const reqBody=await NextRequest.json()
         const {username,email,password} = reqBody
         console.log(`Log: ${reqBody}`)
-        const user = await User.findOne({email})
+        // const user = await User.findOne({email})
         //check if user exists
-        if(user){
-            return NextResponse.json({error: "User already exists"},{status: 400})
-        }
+        // if(user){
+        //     return NextResponse.json({error: "User already exists"},{status: 400})
+        // }
         //hash password
-        const salt = await bcryptjs.genSalt(10)
+        const salt = await bcryptjs.genSalt(2)
         const hashedPassword = await bcryptjs.hash(password,salt)
-        
+        console.log(hashedPassword)
         const newUser = new User({
             username,
             email,
-            hashedPassword
+            password: hashedPassword,
         })
         try{
             await newUser.save()
-            console.log(``)
+            console.log(`saved Succesfully`)
             return NextResponse.json({
                 message: "User created successfully",
                 success: true,
-                savedUser
-            })
+            },{status: 200})
         } catch (error){
-            
+            console.log("Error in signup: "+error)
+            return NextResponse.json({
+                message: "Failed to create new user",
+                sucess: false,
+            },{status: 500})
         }
     }catch(error){
         return NextResponse.json({
